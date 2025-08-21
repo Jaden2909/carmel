c01a4facfdd66a5e4499137bd124a2d7a989a3e7
Jaden2909 <jadenrpaul29@gmail.com>
2025-08-21 20:03:20 +0530

    Update site content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <section className="about-section" id="about">
       <div className="about-text" data-aos="fade-up" data-aos-offset="200">
         <h2>
-          Serving smiles <br /> since <span className="highlight"><u>2013</u></span>
+          Since <span className="highlight"><u>2013</u></span>
         </h2>
         <p>
           Our clinic isn’t just where teeth get fixed. It’s where confidence is built, comfort is prioritized, and technology meets expertise. Explore our range of services — each crafted with precision, care, and a touch of innovation. </p>
